c909368f864d5ab9a90d2f5ff4cc4323e12245ca
Shashwat Aggarwal <shashwat4699@gmail.com>
2020-04-23 19:55:42 +0530

    doctor page

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for
 
 app = Flask(__name__)
-
+app.secret_key = 'we_are_56'
 
 @app.route('/')
 def hello_world():
@@ -16,12 +16,21 @@ def login():
     if usertype == 'patient':
         return render_template("patient.html", username=_username)
     elif usertype == 'doctor':
-        return render_template("doctor.html", username=_username)
+        return render_template("doctor.html", username=_username, name='Shashwat', appointments = [['shashwat', 'sunday 10AM', 'paf'], ['bhunesh', 'tuesday 7PM', 'aslkdfj']])
     elif usertype == 'pharmacy':
         return render_template("pharmacy.html", username=_username)
     elif usertype == 'clinic':
         return render_template("clinic.html", username=_username)
+    else:
+        flash('Please select user type')
+        return redirect(url_for('hello_world'))
+i = 0
+def get_i():
+    global i
+    i+=1
+    return i
 
 
 if __name__ == '__main__':
+    app.jinja_env.globals.update(get_i=get_i)
     app.run(debug = True)
