527baeee0193d4c83891a55b07ebb82a411f1395
Bhunesh <bhunesh18280@iiitd.ac.in>
2020-04-26 23:21:41 +0530

    signup, patient, pharmacy page added

--- app.py
+++ app.py
@@ -5,25 +5,32 @@ app = Flask(__name__)
 app.secret_key = 'we_are_56'
 
 @app.route('/')
-def hello_world():
+def home():
     return render_template("home.html")
 
-
-@app.route('/login', methods=['POST'])
-def login():
+@app.route('/login_signup', methods=['POST'])
+def login_signup():
+    loginorsignup=request.form.get('loginorsignup')
+    print(loginorsignup)
     usertype = request.form.get('usertype')
     _username = request.form.get('username')
-    if usertype == 'patient':
-        return render_template("patient.html", username=_username)
-    elif usertype == 'doctor':
-        return render_template("doctor.html", username=_username, name='Shashwat', appointments = [['shashwat', 'sunday 10AM', 'paf'], ['bhunesh', 'tuesday 7PM', 'aslkdfj']])
-    elif usertype == 'pharmacy':
-        return render_template("pharmacy.html", username=_username)
-    elif usertype == 'clinic':
-        return render_template("clinic.html", username=_username)
+    if (loginorsignup=='login'):
+        if usertype == 'patient':
+            return render_template("patient.html", username=_username,name='Rishab',appointments = [['shashwat', 'sunday 10AM', 'paf'], ['bhunesh', 'tuesday 7PM', 'aslkdfj']])
+        elif usertype == 'doctor':
+            return render_template("doctor.html", username=_username, name='Shashwat', appointments = [['shashwat', 'sunday 10AM', 'paf'], ['bhunesh', 'tuesday 7PM', 'aslkdfj']])
+        elif usertype == 'pharmacy':
+            return render_template("pharmacy.html", username=_username)
+        elif usertype == 'clinic':
+            return render_template("clinic.html", username=_username)
+        else:
+            flash('Please select user type')
+            return redirect(url_for('home'))
+    elif (loginorsignup=='signup'):
+        return render_template("signup.html",usertype=usertype)
     else:
-        flash('Please select user type')
-        return redirect(url_for('hello_world'))
+        flash('Please try again')
+        return redirect(url_for('home'))
 i = 0
 def get_i():
     global i
