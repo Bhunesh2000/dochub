1700c6b1ac23762593babbc158f977605b095d46
Shashwat Aggarwal <shashwat4699@gmail.com>
2020-04-27 17:06:41 +0530

    login signup backend integration

--- login.py
+++ login.py
@@ -5,7 +5,7 @@ mydb = mysql.connector.connect(
     host = "localhost",
     user = "root",
     passwd = "apple212",
-    database = "test"
+    database = "testdata"
     )
 mydbcursor = mydb.cursor()
 
@@ -62,16 +62,22 @@ def login():
         else :
             print('Invalid option')
             
-def validate_login(type, username, password):
+def validate_login(user_type, username, password):
     global mydbcursor
-    query = "select d_id from login_doctors where username='" + username +"' and password='"+password+"';"
+    x_id = ''
+    if(user_type == 'pharmacy'):
+        user_type = 'pharmacies'
+        x = 'pharma'
+    else:
+        user_type += 's'
+        x = user_type[0]
+    query = f"select {x}_id, {x}_name from {user_type} natural join login_{user_type} where username='{username}' and password='{password}';"
     mydbcursor.execute(query)
-    # userids = list(mydbcursor.fetchall())
-    ids = [i[0] for i in mydbcursor.fetchall()]
+    ids = list(mydbcursor.fetchall())
     if(len(ids)==1):
         print('LOGGED IN')
         return ids[0]
-    return 0
+    return (0,0)
 
 def create_user(type, x_id, username, password):
     login_table = 'login_'
@@ -88,73 +94,75 @@ def create_user(type, x_id, username, password):
 
 
 
-def new_clinic(name, address, phone):
+def new_clinic(name, address, phone, username, password):
     global mydb, mydbcursor
     query = f"insert into clinics values(NULL, '{name}','{address}','{phone}')"
-    try:
-        mydbcursor.execute(query)
-        mydb.commit()
-        mydbcursor.execute("select last_insert_id()")
-        c_id = mydbcursor.fetchall()[0][0]
-    except mysql.connector.errors.IntegrityError as e:
-        print(e)
-    # create_user('clinic', c_id, 'username', 'password')
+    mydbcursor.execute(query)
+    mydb.commit()
+    mydbcursor.execute("select last_insert_id()")
+    c_id = mydbcursor.fetchall()[0][0]
+    query = f"insert into login_clinics values(NULL, {c_id}, '{username}', '{password}')"
+    mydbcursor.execute(query)
+    mydb.commit()
+    return c_id
 
 
-def new_doctor(name, specialization, fee, phone):
+def new_doctor(name, specialization, fee, phone, username, password):
     global mydb, mydbcursor
     query = f"insert into doctors values(NULL, '{name}','{specialization}', '{fee}','{phone}')"
-    try:
-        mydbcursor.execute(query)
-        mydb.commit()
-        mydbcursor.execute("select last_insert_id()")
-        d_id = mydbcursor.fetchall()[0][0]
-        #create user account
-    except mysql.connector.errors.IntegrityError as e:
-        print(e)
+    mydbcursor.execute(query)
+    mydb.commit()
+    mydbcursor.execute("select last_insert_id()")
+    d_id = mydbcursor.fetchall()[0][0]
+    query = f"insert into login_doctors values(NULL, {d_id}, '{username}', '{password}')"
+    mydbcursor.execute(query)
+    mydb.commit()
+    return d_id        
 
-def new_patient(name, sex, age, phone):
+def new_patient(name, sex, age, phone, username, password):
     global mydb, mydbcursor
     query = f"insert into patients values(NULL, '{name}','{sex}', {age},'{phone}')"
-    try:
-        mydbcursor.execute(query)
-        mydb.commit()
-        mydbcursor.execute("select last_insert_id()")
-        d_id = mydbcursor.fetchall()[0][0]
-        #create user account
-    except mysql.connector.errors.IntegrityError as e:
-        print(e)
+    mydbcursor.execute(query)
+    mydb.commit()
+    mydbcursor.execute("select last_insert_id()")
+    p_id = mydbcursor.fetchall()[0][0]
+    query = f"insert into login_patients values(NULL, {p_id}, '{username}', '{password}')"
+    mydbcursor.execute(query)
+    mydb.commit()
+    return p_id
 
-def new_pharmacy(name, phone):
+def new_pharmacy(name, phone, username, password):
     global mydb, mydbcursor
     query = f"insert into pharmacies values(NULL, '{name}', '{phone}')"
-    try:
-        mydbcursor.execute(query)
-        mydb.commit()
-        mydbcursor.execute("select last_insert_id()")
-        d_id = mydbcursor.fetchall()[0][0]
-        #create user account
-    except mysql.connector.errors.IntegrityError as e:
-        print(e)
+    mydbcursor.execute(query)
+    mydb.commit()
+    mydbcursor.execute("select last_insert_id()")
+    pharma_id = mydbcursor.fetchall()[0][0]
+    query = f"insert into login_pharmacies values(NULL, {pharma_id}, '{username}', '{password}')"
+    mydbcursor.execute(query)
+    mydb.commit()
+    return pharma_id
 
 def view_doc_upcoming_appointments(d_id, c_id = 0):
     global mydb, mydbcursor
     if(c_id):
-        query = f"select * from appointments where d_id = {d_id} and c_id = {c_id} and schedule >= now()"
+        query = f"select a_id, p_id, p_name, c_name, schedule, description from appointments natural join clinics natural join patients where d_id = {d_id} and c_id = {c_id} and schedule >= now()"
     else:
-        query = f"select * from appointments where d_id = {d_id} and schedule >= now()"
+        query = f"select a_id, p_id, p_name, c_name, schedule, description from appointments natural join clinics natural join patients where d_id = {d_id} and schedule >= now()"
     mydbcursor.execute(query)
-    appointments = list(mydbcursor.fetchall())
+    appointments = map(lambda x: x[:-2] + (x[-2].strftime("%d-%b-%Y %I:%M"),x[-1]) , mydbcursor.fetchall())
+    appointments = list(appointments)
     return appointments
 
 def view_doc_past_appointments(d_id, c_id = 0):
     global mydb, mydbcursor
     if(c_id):
-        query = f"select * from appointments where d_id = {d_id} and c_id = {c_id} and schedule < now()"
+        query = f"select a_id, p_id, p_name, c_name, schedule, description from appointments natural join clinics natural join patients where d_id = {d_id} and c_id = {c_id} and schedule < now()"
     else:
-        query = f"select * from appointments where d_id = {d_id} and schedule < now()"
+        query = f"select a_id, p_id, p_name, c_name, schedule, description from appointments natural join clinics natural join patients where d_id = {d_id} and schedule < now()"
     mydbcursor.execute(query)
-    appointments = list(mydbcursor.fetchall())
+    appointments = map(lambda x: x[:-2] + (x[-2].strftime("%d-%b-%Y %I:%M"),x[-1]) , mydbcursor.fetchall())
+    appointments = list(appointments)
     return appointments
 
 def view_docs():
@@ -181,16 +189,18 @@ def clinic_remove_doc(c_id, d_id):
 
 def view_patient_upcoming_appointments(p_id):
     global mydb, mydbcursor
-    query = f"select * from appointments where p_id = {p_id} and schedule >= now()"
+    query = f"select a_id, c_name, d_name, schedule, description from appointments natural join doctors natural join clinics where p_id = {p_id} and schedule >= now()"
     mydbcursor.execute(query)
-    appointments = list(mydbcursor.fetchall())
+    appointments = map(lambda x: x[:-2] + (x[-2].strftime("%d-%b-%Y %I:%M"),x[-1]) , mydbcursor.fetchall())
+    appointments = list(appointments)
     return appointments
 
 def view_patient_past_appointments(p_id):
     global mydb, mydbcursor
-    query = f"select * from appointments where p_id = {p_id} and schedule < now()"
+    query = f"select a_id, c_name, d_name, schedule, description from appointments natural join doctors natural join clinics where p_id = {p_id} and schedule < now()"
     mydbcursor.execute(query)
-    appointments = list(mydbcursor.fetchall())
+    appointments = map(lambda x: x[:-2] + (x[-2].strftime("%d-%b-%Y %I:%M"),x[-1]) , mydbcursor.fetchall())
+    appointments = list(appointments)
     return appointments
 
 
@@ -220,6 +230,12 @@ def show_schedules(d_id):
     query = f"select * from schedules where d_id = {d_id}"
     mydbcursor.execute(query)
     return list(mydbcursor.fetchall())
+
+def add_clinic_timing(c_id, day, start, end):
+    global mydb, mydbcursor
+    query = f"insert into clinic_timings values({c_id},'{day}','{start}', '{end}')"
+    mydbcursor.execute(query)
+    mydb.commit()
 
 def pharma_manageProfile(pharma_id,type,new):
     global mydb, mydbcursor
@@ -288,6 +304,13 @@ def doctorSchedule(d_id,c_id,task):
         query=f"insert into schedules (d_id, c_id, day, start, end) VALUES ('{d_id}', '{c_id}', '{newDay}', '{newStart}', '{newEnd}')"
     mydbcursor.execute(query)
     mydb.commit()
+
+def get_prescriptions(p_username):
+    global mydb, mydbcursor
+    query = f"select meds from prescriptions natural join login_patients where username = '{p_username}' and prescription_id = (select max(prescription_id) from prescriptions natural join login_patients where username = '{p_username}')"
+    # lect description from (SELECT a_id, description from appointments natural join login_patients where username = 'shashwat90') t where a_id = (SELECT MAX(a_id) FROM appointments natural join login_patients where username = '{p_username}')
+    mydbcursor.execute(query)
+    return list(mydbcursor.fetchall())
 
 if __name__ == "__main__":
     
