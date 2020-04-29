45bd8fda837b6739912141a44f4ba668c0baaca0
Bhunesh <bhunesh18280@iiitd.ac.in>
2020-04-29 17:44:07 +0530

    signup and schedule/timings changed

--- login.py
+++ login.py
@@ -4,7 +4,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host = "localhost",
     user = "root",
-    passwd = "apple212",
+    passwd = "dbms56",
     database = "testdata"
     )
 mydbcursor = mydb.cursor()
